14402424e91a4e9de7e904b597927ae50f1a1570
grigoreansilviu <silviu.grigorean01@e-uvt.ro>
2022-04-21 13:01:46 +0300

    21.04.2022

--- Project/magazin.php
+++ Project/magazin.php
@@ -4,21 +4,68 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/boostraq/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/boostraq/css/bootstrap.css"  >
+
 
     <title>Hello, world!</title>
 </head>
 <body>
-<div class="row">
-    <div class="col-md-4">
-        <h2>Hi there</h2>
-    </div>
-    <div class="col-md-6">
-        <h2>Hello</h2>
-    </div>
-</div>
+<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
 
+            <img class="imagine navbar-brand mb-0 h1" src="assets/images/logo.png" />
+
+        <button type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                class="navbar-toggler"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Taggle navigaion">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link active">Acasa</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle"
+                       id="navbarDropdown"
+                       role="button"
+                       data-bs-toggle="dropdown"
+                       aria-expanded="false">Contul meu</a>
+                    <ul class="dropdown-menu"
+                        aria-labelledby="navbarDropdown">
+                        <li>
+                            <a href="#" class="dropdown-item">Intra in cont</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">Acasa</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">Practica</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">Teprie</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">Magazin</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Despre</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
+<script src="assets/boostraq/js/bootstrap.js" ></script>
 </body>
 </html>
