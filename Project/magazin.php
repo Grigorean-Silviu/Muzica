2142edfdb3a9390333ed0e953ad73801e5c2c67b
grigoreansilviu <silviu.grigorean01@e-uvt.ro>
2022-04-21 14:09:43 +0300

    21.04.2022

--- Project/magazin.php
+++ Project/magazin.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="assets/css/index.css">
@@ -18,47 +17,33 @@
 
             <img class="imagine navbar-brand mb-0 h1" src="assets/images/logo.png" />
 
-        <button type="button"
+        <button
+                type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#navbarNav"
                 class="navbar-toggler"
                 aria-controls="navbarNav"
                 aria-expanded="false"
-                aria-label="Taggle navigaion">
-            <span class="navbar-toggler-icon"></span>
+                aria-label="Toggle navigation">
         </button>
+
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
+
+
                 <li class="nav-item active">
-                    <a href="index.php" class="nav-link active">Acasa</a>
+                    <a href="#" class="nav-link ">Contul meu</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle"
-                       id="navbarDropdown"
-                       role="button"
-                       data-bs-toggle="dropdown"
-                       aria-expanded="false">Contul meu</a>
-                    <ul class="dropdown-menu"
-                        aria-labelledby="navbarDropdown">
-                        <li>
-                            <a href="#" class="dropdown-item">Intra in cont</a>
-                        </li>
-                        <li>
-                            <a href="#" class="dropdown-item">Acasa</a>
-                        </li>
-                        <li>
-                            <a href="#" class="dropdown-item">Practica</a>
-                        </li>
-                        <li>
-                            <a href="#" class="dropdown-item">Teprie</a>
-                        </li>
-                        <li>
-                            <a href="#" class="dropdown-item">Magazin</a>
-                        </li>
-                    </ul>
+                <li class="nav-item active">
+                    <a href="contact.php" class="nav-link ">Contact</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Despre</a>
+                <li class="nav-item active">
+                    <a href="despre.php" class="nav-link ">Despre</a>
+                </li>
+
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link ">Acasa</a>
                 </li>
             </ul>
         </div>
--- Project/practica.php
+++ Project/practica.php
@@ -1,11 +1,71 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Practica la instrumente de suflat</title>
-    <meta name="description" content="Timpul de practica si exercitii la instrumentele de suflat">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/boostraq/css/bootstrap.css"   crossorigin="anonymous">
+
+    <title>Hello, world!</title>
 </head>
 <body>
+<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
+
+        <img class="imagine navbar-brand mb-0 h1" style="width: 48px" src="assets/images/logo.png" alt="logo" />
+
+        <button
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                class="navbar-toggler"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle"
+                    id="navbarDropdown " role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false">Contul meu</a>
+                    <ul class="dropdown-menu "  aria-labelledby="navbarDropdown ">
+                        <li>
+                            <a href="#" class="dropdown-item">Intra in cont</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">Teorie</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item">Practica</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item active">
+                    <a href="contact.php" class="nav-link ">Contact</a>
+                </li>
+                <li class="nav-item active">
+                    <a href="despre.php" class="nav-link ">Despre</a>
+                </li>
+
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link ">Acasa</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
